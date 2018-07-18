e0105a82932c9c7a82c44dae53ab9a07157d0ccd
Georgi Popov <georgipopov88@gmail.com>
2018-07-18 21:43:18 +0300

    ListyIterator - done.

--- src/iterators_and_comparatotrs/exercises/p01_ListyIterator/ListyIterator.java
+++ src/iterators_and_comparatotrs/exercises/p01_ListyIterator/ListyIterator.java
@@ -3,57 +3,57 @@ package iterators_and_comparatotrs.exercises.p01_ListyIterator;
 import java.util.Iterator;
 
 public class ListyIterator<T> implements Iterable<T> {
- private T[] items;
- private int index;
- 
- 
- @SafeVarargs
- ListyIterator(T... elements) {
+private T[] items;
+private int index;
+
+
+@SafeVarargs
+ListyIterator (T... elements) {
   this.items = elements;
   this.index = 0;
- }
- 
- public boolean move() {
-  if (hasNext()) {
-   this.index++;
-   return true;
+}
+
+public boolean move () {
+  if (hasNext ()) {
+	this.index++;
+	return true;
   } else {
-   return false;
+	return false;
   }
- }
- 
- public boolean hasNext() {
+}
+
+public boolean hasNext () {
   return this.index < items.length - 1;
- }
- 
- public void print() {
+}
+
+public void print () {
   if (items != null && items.length > 0) {
-   System.out.println(items[this.index]);
+	System.out.println (items[this.index]);
   } else {
-   System.out.println("Invalid Operation!");
+	System.out.println ("Invalid Operation!");
   }
- }
- 
- @Override
- public Iterator<T> iterator() {
-  return new MyIterator();
- }
- 
- private final class MyIterator implements Iterator<T> {
+}
+
+@Override
+public Iterator<T> iterator () {
+  return new MyIterator ();
+}
+
+private final class MyIterator implements Iterator<T> {
   private int index;
-  
-  private MyIterator() {
-   this.index = 0;
+
+  private MyIterator () {
+	this.index = 0;
   }
-  
+
   @Override
-  public boolean hasNext() {
-   return this.index < items.length;
+  public boolean hasNext () {
+	return this.index < items.length;
   }
- 
+
   @Override
-  public T next() {
-   return items[index++];
+  public T next () {
+	return items[index++];
   }
- }
+}
 }
--- src/iterators_and_comparatotrs/exercises/p01_ListyIterator/Main.java
+++ src/iterators_and_comparatotrs/exercises/p01_ListyIterator/Main.java
@@ -6,39 +6,39 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
- public static void main(String[] args) throws IOException {
-  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+public static void main (String[] args) throws IOException {
+  BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
   ListyIterator<String> listyIterator = null;
-  
+
   while (true) {
-   String inLine = reader.readLine();
-   
-   if ("END".equals(inLine)) {
-    break;
-   }
-   
-   String[] inTokens = inLine.split("\\s+");
-   
-   switch (inTokens[0]) {
-    case "Create":
-     String[] arguments = Arrays.stream(inTokens).skip(1).toArray(String[]::new);
-     listyIterator = new ListyIterator<>(arguments);
-     break;
-    case "HasNext":
-     System.out.println(listyIterator.hasNext());
-     break;
-    case "Move":
-     System.out.println(listyIterator.move());
-     break;
-    case "Print":
-     listyIterator.print();
-     break;
-    case "PrintAll":
-     System.out.println(String.join(" ", listyIterator));
-     break;
-    
-   }
-   
+	String inLine = reader.readLine ();
+
+	if ("END".equals (inLine)) {
+	  break;
+	}
+
+	String[] inTokens = inLine.split ("\\s+");
+
+	switch (inTokens[0]) {
+	  case "Create":
+		String[] arguments = Arrays.stream (inTokens).skip (1).toArray (String[] ::new);
+		listyIterator = new ListyIterator<> (arguments);
+		break;
+	  case "HasNext":
+		System.out.println (listyIterator.hasNext ());
+		break;
+	  case "Move":
+		System.out.println (listyIterator.move ());
+		break;
+	  case "Print":
+		listyIterator.print ();
+		break;
+	  case "PrintAll":
+		System.out.println (String.join (" ", listyIterator));
+		break;
+
+	}
+
   }
- }
+}
 }
