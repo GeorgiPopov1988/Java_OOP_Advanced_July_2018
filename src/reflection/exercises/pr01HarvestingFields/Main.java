106bc592675216c5692b5d773a7aaf6658e1f6dc
Georgi Popov <georgipopov88@gmail.com>
2018-07-25 00:36:53 +0300

    Harvesting fields - done.

---
+++ src/reflection/exercises/pr01HarvestingFields/Main.java
@@ -0,0 +1,64 @@
+package reflection.exercises.pr01HarvestingFields;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class Main {
+
+public static void main(String[] args) throws IOException {
+  
+  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  
+  String modifier;
+  while (true) {
+    if ("HARVEST".equals(modifier = reader.readLine())) {
+      break;
+    }
+  
+    Field[] fields = getFields(modifier);
+    printFields(fields);
+  }
+  
+  
+}
+
+private static void printFields(Field[] fields) {
+  
+  Arrays.stream(fields)
+          .forEach(field ->
+                  System.out.println(String.format("%s %s %s",
+                          Modifier.toString(field.getModifiers()),
+                          field.getType().getSimpleName(),
+                          field.getName())));
+
+}
+
+private static Field[] getFields(String modifier) {
+  Field[] fields = null;
+  switch (modifier) {
+    case "public":
+      fields = Arrays.stream(RichSoilLand.class.getDeclaredFields())
+              .filter(field -> Modifier.isPublic(field.getModifiers()))
+              .toArray(Field[]::new);
+      break;
+    case "protected":
+      fields = Arrays.stream(RichSoilLand.class.getDeclaredFields())
+              .filter(field -> Modifier.isProtected(field.getModifiers()))
+              .toArray(Field[]::new);
+      break;
+    case "private":
+      fields = Arrays.stream(RichSoilLand.class.getDeclaredFields())
+              .filter(field -> Modifier.isPrivate(field.getModifiers()))
+              .toArray(Field[]::new);
+      break;
+    case "all":
+      fields = RichSoilLand.class.getDeclaredFields();
+      break;
+  }
+  return fields;
+}
+}
